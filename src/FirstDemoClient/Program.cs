b6045d4cc3d6234ff82366b843143ad687e3afc6
monte <greg.jonca@gmail.com>
2019-07-07 11:30:33 +0200

    Add simple projects showing gRPC

---
+++ src/FirstDemoClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using FirstDemo;
+using Grpc.Net.Client;
+using System;
+using System.Net.Http;
+
+namespace FirstDemoClient
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            AppContext.SetSwitch(
+    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport",
+    true);
+            var httpClient = new HttpClient();
+            // The port number(50051) must match the port of the gRPC server.
+            httpClient.BaseAddress = new Uri("http://localhost:50051");
+            var client = GrpcClient.Create<FirstDemo.Greeter.GreeterClient>(httpClient);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
