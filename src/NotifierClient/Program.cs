82f3e2307c1085bbb76199631b62acd1d6b8599e
devmonte <greg.jonca@gmail.com>
2019-11-13 10:36:49 +0100

    Add Notifier example

---
+++ src/NotifierClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Notifier;
+
+namespace NotifierClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Notifier.Notifier.NotifierClient(grpcChannel);
+            var reply = await client.NotifyAsync(new NotificationRequest
+                {Id = 666, Content = "Hello from small client!"});
+
+            Console.WriteLine($"Reply: {reply.Result}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/NotifierService/Services/NotifierService.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using Notifier;
+
+namespace NotifierService.Services
+{
+    public class NotifierService : Notifier.Notifier.NotifierBase
+    {
+        private readonly ILogger<NotifierService> _logger;
+
+        public NotifierService(ILogger<NotifierService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<NotificationReply> Notify(NotificationRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation($"Received request: {request.Id}  with content: {request.Content}");
+            return Task.FromResult(new NotificationReply { Result = "Success" });
+        }
+    }
+}
