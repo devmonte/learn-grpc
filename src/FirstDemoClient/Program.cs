bc3581a9decb45efc0c7bacc8be07c8082d6b6e9
devmonte <greg.jonca@gmail.com>
2019-11-13 09:36:28 +0100

    Update first demo project and solution structure

--- src/FirstDemoClient/Program.cs
+++ src/FirstDemoClient/Program.cs
@@ -11,16 +11,21 @@ namespace FirstDemoClient
         {
             Console.WriteLine("Hello World!");
 
-            AppContext.SetSwitch(
-    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport",
-    true);
-            var httpClient = new HttpClient();
-            // The port number(50051) must match the port of the gRPC server.
-            httpClient.BaseAddress = new Uri("http://localhost:50051");
-            var client = GrpcClient.Create<FirstDemo.Greeter.GreeterClient>(httpClient);
-            var reply = await client.SayHelloAsync(
-                              new HelloRequest { Name = "GreeterClient" });
-            Console.WriteLine("Greeting: " + reply.Message);
+            //AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            //var httpClient = new HttpClient();
+            //// The port number(50051) must match the port of the gRPC server.
+            //httpClient.BaseAddress = new Uri("http://localhost:50051");
+            //var client = GrpcClient.Create<FirstDemo.Greeter.GreeterClient>(httpClient);
+            //var reply = await client.SayHelloAsync(
+            //                  new HelloRequest { Name = "GreeterClient" });
+            //Console.WriteLine("Greeting: " + reply.Message);
+            
+            var channel = GrpcChannel.ForAddress("https://localhost:50051");
+            var client = new Greeter.GreeterClient(channel);
+
+            var response = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+            Console.WriteLine(response.Message);
+            
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
 
